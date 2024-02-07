40fb2dde15b957496e99cede0d3bf0268663f81e
CurryCalculus <124116098+CurryCalculus@users.noreply.github.com>
2024-02-07 20:04:19 +1300

    Exercise 1

---
+++ Lvl_1_Recap_Ex_1.py
@@ -0,0 +1,17 @@
+while True:
+    num_1 = int(input("Enter a number (Choose 0 to exit): "))
+
+    if num_1 == 0:
+        print("You have exited this thing")
+        break
+
+    num_2 = int(input("Enter the second number: "))
+
+    if num_1 > num_2:
+        result = num_1
+    elif num_2 > num_1:
+        result = num_2
+    else:
+        print("The numbers are equal.")
+
+    print(f"The higher value is: {result}")
